bcffe7b23889c040d5308617bc6ff7b88f998163
abbeynism2 <135938880+abbeynism2@users.noreply.github.com>
2025-06-08 12:25:47 +0100

    Add .next/static/chunks/src_app_contact_page_tsx_0ef0cac3._.js 1749381948378

---
+++ .next/static/chunks/src_app_contact_page_tsx_0ef0cac3._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_next_d2ed38d8._.js",
+  "static/chunks/src_app_contact_page_tsx_20db083e._.js"
+],
+    source: "dynamic"
+});
